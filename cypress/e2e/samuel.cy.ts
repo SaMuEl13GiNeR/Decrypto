c6fe9ec57e65db4d68cdacdb60965e4de8f0a2fe
SaMuEl13GiNeR <sam.gi2002@gmail.com>
2023-03-22 12:41:07 +0100

    tests acabats i corregits

--- cypress/e2e/samuel.cy.ts
+++ cypress/e2e/samuel.cy.ts
@@ -8,23 +8,32 @@ describe('tests cypress en decypto', () => {
   });
 
   it('2. Que el primer elemento de la lista sea el que mayor capitalización de mercado tenga', () => {
-    // const capitalizations = [];
-    // cy.get('.item td:nth-child(5)').should('be.a', 'string');
+    cy.get('.item .market-cap').each(($number, $index, $list) => {
+      let $number1 = parseFloat(
+        $number.text().trim().replaceAll(',', '').slice(1)
+      );
+      if ($index != $list.length - 1) {
+        let $number2 = parseFloat(
+          $list[$index + 1].innerText.trim().replaceAll(',', '').slice(1)
+        );
+        expect($number1).be.gte($number2);
+      }
+    });
   });
 
   it('3. Que el cambio de precio durante las 24 últimas horas aparezca verde si es positivo o rojo si es negativo', () => {
-    cy.get('.item td:nth-child(6) span p').should(($color) => {
-      if (parseFloat($color.text()) > 0) {
-        expect($color).to.have.class('text-success');
+    cy.get('.item .price_change_percentage_24h span p').each(($color) => {
+      if (parseFloat($color.text().trim().replace('%', '')) >= 0) {
+        expect($color.parent()).to.have.class('text-success');
       } else {
-        expect($color).to.have.class('text-danger');
+        expect($color.parent()).to.have.class('text-danger');
       }
     });
   });
 
   it('4. Comprobar que junto al precio de cada criptomoneda salga el símbolo del dólar', () => {
-    cy.get('.item td:nth-child(4)')
-      .invoke('text')
-      .then((text) => expect(text.trim()).to.match(/^\$/));
+    cy.get('.item .price').each(($price) => {
+      expect($price.text().trim()).to.match(/^\$/);
+    });
   });
 });
